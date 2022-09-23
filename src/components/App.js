3b2a83bca4d4ec039758e32474b18c683c49faff
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-09-23 15:51:34 -0400

    update nested routes

--- src/components/App.js
+++ src/components/App.js
@@ -7,38 +7,37 @@ import C from './C.js';
 import Item from './Item.js';
 import Person from './Person.js';
 import NoMatch from './NoMatch.js';
-import {Routes, Route} from 'react-router-dom';
-import {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function App() {
-  const [cast, setCast] = useState([
-    {id:12, character:'Sheldon', actor:'Jim Parsons'},
-    {id:34, character:'Leonard', actor:'Johnny Galecki'},
-    {id:56, character:'Penny', actor:'Kaley Cuoco'},
-    {id:78, character:'Amy', actor:'Miyam Bialik'},
-    {id:90, character:'Bernadette', actor:'Melissa Rauch'},
+  const [cast] = useState([
+    { id: 12, character: 'Sheldon', actor: 'Jim Parsons' },
+    { id: 34, character: 'Leonard', actor: 'Johnny Galecki' },
+    { id: 56, character: 'Penny', actor: 'Kaley Cuoco' },
+    { id: 78, character: 'Amy', actor: 'Miyam Bialik' },
+    { id: 90, character: 'Bernadette', actor: 'Melissa Rauch' },
   ]);
 
   return (
-    <div className='app'>
+    <div className="app">
       <header>
         <h1>React Router</h1>
       </header>
       <Nav />
       <main>
         <Routes>
-          <Route path="/" element={<Home />}>
+          <Route path="/*" element={<Home />}>
             <Route path="a" element={<A />} />
             <Route path="b" element={<B cast={cast} />} />
             <Route path="b/:cid" element={<Person cast={cast} />} />
-            <Route path="c" element={<C />}>
-              <Route path=":id" element={<Item/>} />
+            <Route path="c/*" element={<C />}>
+              <Route path=":id" element={<Item />} />
             </Route>
           </Route>
           <Route path="*" element={<NoMatch />} />
-        </Routes> 
+        </Routes>
       </main>
     </div>
   );
 }
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import './home.css';
 
-export default function Home(){
-
+export default function Home() {
   return (
     <section className="home">
       <h1>Welcome Home</h1>
-      
-      <Outlet />
 
+      <Outlet />
     </section>
-  )
+  );
 }
